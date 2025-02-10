3c2c91d27a2c8000db8bf6156cce993d7cbe18fc
Var-Guz <davidvarguz.7@unitec.edu>
2025-02-09 23:15:44 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioHerencia_Angel/Operaciones.cs
@@ -0,0 +1,21 @@
+﻿
+namespace EjercicioHerencia_Angel
+{
+    public class Operaciones
+    {
+        public double AreaCuadrado(double lado)
+        {
+            return lado * lado;
+        }
+        public double LongitudCirculo(double radio)
+        {
+            return 2 * Math.PI * radio;
+        }
+        public double VolumenCubo(double lado)
+        {
+            return lado * lado *lado;
+        }
+
+    }
+
+}
---
+++ EjercicioHerencia_Angel/OperacionesHerencias.cs
@@ -0,0 +1,19 @@
+﻿
+namespace EjercicioHerencia_Angel
+{
+    public class OperacionesHerencias : Operaciones
+    {
+        public void Imprimir()
+        {
+            Console.WriteLine("Area del cuadrado: " + AreaCuadrado(5));
+            Console.WriteLine("Area del cuadrado: " + AreaCuadrado(5));
+            Console.WriteLine("Area del cuadrado: " + AreaCuadrado(5));
+
+
+
+        }
+
+            
+
+    }
+}
